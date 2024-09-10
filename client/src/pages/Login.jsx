82a67b90dd14894d78512f01d296afbe2fdf9094
rob@rwmconsultancy.nl <rob@rwmconsultancy.nl>
2024-09-10 15:05:01 +0200

    REmoved defaultvalues

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -38,8 +38,8 @@ const Login = () => {
       <Form method="post" className="form">
         <Logo />
         <h4>Login</h4>
-        <FormRow type="email" name="email" defaultValue="john@mail.com" />
-        <FormRow type="password" name="password" defaultValue="secret123" />
+        <FormRow type="email" name="email" />
+        <FormRow type="password" name="password" />
         <SubmitBtn />
         <button type="button" className="btn btn-block" onClick={loginDemo}>
           Explore the app
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -27,16 +27,16 @@ const Register = () => {
       <Form method="post" className="form">
         <Logo />
         <h4>Register</h4>
-        <FormRow type="text" name="name" defaultValue="John" />
+        <FormRow type="text" name="name" />
         <FormRow
           type="text"
           name="lastName"
           labelText="Last name"
           defaultValue="Smith"
         />
-        <FormRow type="text" name="location" defaultValue="Earth" />
-        <FormRow type="email" name="email" defaultValue="john@mail.com" />
-        <FormRow type="password" name="password" defaultValue="secret123" />
+        <FormRow type="text" name="location" />
+        <FormRow type="email" name="email" />
+        <FormRow type="password" name="password" />
         <SubmitBtn />
         <p>
           Already a member?
